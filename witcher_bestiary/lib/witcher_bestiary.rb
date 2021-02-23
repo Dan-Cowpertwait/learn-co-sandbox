69097ad1023140572c7a4ec0a7913b0c4a877905
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2021-02-23 21:58:46 +0000

    Automatically backed up by Learn

--- witcher_bestiary/lib/witcher_bestiary.rb
+++
@@ -1,6 +0,0 @@
-require "witcher_bestiary/version"
-
-module WitcherBestiary
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- witcher_bestiary/lib/witcher_bestiary/version.rb
+++
@@ -1,3 +0,0 @@
-module WitcherBestiary
-  VERSION = "0.1.0"
-end
